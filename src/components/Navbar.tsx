330f0dc4b438d64f8e4c97b71e9ce1ae8266dc08
Belgutei Byambadorj <z5458085@ad.unsw.edu.au>
2025-12-20 13:01:26 +1100

    added responsive layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router";
 
 export default function Navbar() {
   return (
-    <div className="w-1/2 h-screen flex flex-col mx-auto">
+    <div className="w-full md:w-5/8 xl:w-3/8 h-screen flex flex-col mx-auto">
       <nav className="w-full mb-3 flex justify-between pt-5 pb-5 border-b border-stone-300">
         <div className="font-medium text-lg text-stone-700">
           <NavLink to="/" end>
