afbf3c79738046275067c25274e8d012c36980ab
Belgutei Byambadorj <z5458085@ad.unsw.edu.au>
2026-01-04 11:18:24 +1100

    Project component

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function MainLayout() {
   return (
-    <div className="w-full md:w-5/8 xl:w-3/8 h-screen flex flex-col mx-auto">
+    <div className="w-full md:w-5/8 2xl:w-4/8 h-screen flex flex-col mx-auto">
       <Navbar />
       <Outlet />
     </div>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,25 +1,23 @@
+import Project from "../components/Project";
+
 export default function Projects() {
   return (
-    <div>
-      <div className="w-80 border-black border-solid border-2">
-        <img src="/projects/wrapper.png" className="h-50 aspect-video" />
-        <ul className="flex space-x-1.5 text-base p-1.5 border-b-2">
-          <li>Next.js</li>
-          <li>TypeScript</li>
-          <li>PostgreSQL</li>
-          <li>Auth</li>
-        </ul>
-        <div className="p-2">
-          <div className="text-xl">Year-Wrapped</div>
-          <div className="pt-4">
-            Description Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-          </div>
-          <div className="flex gap-2 mt-4">
-            <div className="project-link">Live</div>
-            <div className="project-link">Github</div>
-          </div>
-        </div>
-      </div>
+    /*
+    Projects
+    1. year-wrapped
+    2. personal-web-v2
+    3. 3900 project
+    4. 6447 fuzzer
+    5. iscm project
+    
+    - short blog for each project
+    */
+    <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 justify-items-stretch space-y-8 pb-10">
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
     </div>
   );
 }
