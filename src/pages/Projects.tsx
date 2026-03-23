0853e7777bc57479beaa5468ac88e15aa2d73ea2
Belgutei <z5458085@ad.unsw.edu.au>
2026-03-23 12:58:27 +1000

    updated descriptions of the projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,31 +12,33 @@ type Project = {
 
 const projects: Project[] = [
   {
-    title: "Dvine Experiences",
-    description:
-      "A full-stack web application for Dvine Experiences to manage partner applications and host a directory of local businesses in Port Macquire. I worked in a team of 6, leading back-end development. I implemented authentication/autheroziation layers, form submission workflow, and cloud infrastructure setup (S3 for image/video storage and retrieval, and SES for email notification to admins and partners).",
-    techs: ["Node.js", "TypeScript", "PostgreSQL", "Auth", "AWS"],
-  },
-  {
     title: "CPTracker",
-    description: "A time focused LeetCode tracker website.",
-    techs: ["React", "Next.js", "Prisma"],
+    description:
+      "A web application for tracking LeetCode sessions. Focusing only on the number of solved problems makes it easy to lose motivation, especially when trying hard questions. CPTracker tracks the time you spend on each problem so you can actually see your effort. An analytics page shows the total time spent each day and the duration it took to solve each problem.",
+    techs: ["React", "Next.js", "PostgreSQL", "Prisma"],
     github: "https://github.com/belgutei-b/cptracker",
     live: "https://www.cptracker.org/",
   },
   {
     title: "CPTracker Chrome Extension",
-    description: "",
-    techs: ["Typescript"],
+    description:
+      "Extension support for CPTracker.org. Start/stop the timer and write notes directly from a pop-up without leaving the LeetCode tab. Once done, the session is saved to your CPTracker account and reflected in your analytics.",
+    techs: ["React", "Typescript", "Plasmo"],
     github: "https://github.com/belgutei-b/cptracker-extension",
     live: "https://chromewebstore.google.com/detail/ojpjlobnleonmgehlhoibaicokoadcnm?utm_source=item-share-cb",
   },
   {
     title: "Safety Website for a construction company",
     description:
-      "A full-stack web application for a construction company to manage pre-start safety talks. This platform enabled managers to assign workers to job sites, while workers could review safety guidelines and digitally sign acknowledgements.",
+      "A full-stack web application for a construction company to manage pre-start safety talks. This platform enabled managers to assign workers to job sites, while workers could review safety guidelines and digitally sign pre-start tool box talk.",
     techs: ["Next.js", "TypeScript", "MongoDB", "AWS"],
     github: "https://github.com/belgutei-b/2safety",
+  },
+  {
+    title: "Dvine Experiences | Business Portal",
+    description:
+      "A full-stack web application for Dvine Experiences to manage partner applications and host a directory of local businesses in Port Macquarie. I worked in a team of 6, leading back-end development. I implemented authentication/authorization layers, form submission workflow, and cloud infrastructure setup (S3 for image/video storage and retrieval, and SES for email notifications to admins and partners).",
+    techs: ["Node.js", "TypeScript", "PostgreSQL", "AWS"],
   },
 ];
 
@@ -98,7 +100,7 @@ export default function Projects() {
       className="px-2 md:px-0"
       style={{ fontFamily: "'JetBrains Mono', monospace" }}
     >
-      <p className="mb-4">Projects</p>
+      <p className="mb-4 text-xl">Projects</p>
       <div className="flex flex-col gap-4">
         {projects.map((project) => (
           <ProjectCard key={project.title} project={project} />
